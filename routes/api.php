f282aa5eb2331fa4710f1db609973be6b44db030
delacroix <delacroix.jon@gmail.com>
2026-05-18 17:04:25 +0200

    Activité 3

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,22 @@
 <?php
 
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', function () {
-    return response()->json([
-        'message' => 'pong'
-    ]);
-});
+/* Auth routes */
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
 
-Route::apiResource('books', BookController::class);
+/* Public book routes */
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{book}', [BookController::class, 'show']);
+
+/* Protected routes */
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::post('/books', [BookController::class, 'store']);
+    Route::put('/books/{book}', [BookController::class, 'update']);
+    Route::patch('/books/{book}', [BookController::class, 'update']);
+    Route::delete('/books/{book}', [BookController::class, 'destroy']);
+});
